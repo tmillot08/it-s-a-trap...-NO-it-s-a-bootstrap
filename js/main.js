2655c79d89a510a109c02c039f531ae4f9d543eb
Inseri <39262186+Inseri@users.noreply.github.com>
2018-05-18 15:08:46 +0200

    Update main.js
    ajout jquery

--- js/main.js
+++ js/main.js
@@ -5,3 +5,42 @@ function on() {
 function off() {
     document.getElementById("overlay").style.display = "none";
 }
+
+$(document).ready (function(){;
+
+$("#talla").click(function(){
+if(cotal.style.display == "none"){
+    $("#cotal").show();
+	}
+	else {
+	$("#cotal").hide();
+	}
+}); 
+
+$("#thomas").click(function(){
+if(cotho.style.display == "none"){
+    $("#cotho").show();
+	}
+	else {
+	$("#cotho").hide();
+	}
+}); 
+
+$("#ingrid").click(function(){
+if(coing.style.display == "none"){
+    $("#coing").show();
+	}
+	else {
+	$("#coing").hide();
+	}
+}); 
+
+$("#anthony").click(function(){
+if(coant.style.display == "none"){
+    $("#coant").show();
+	}
+	else {
+	$("#coant").hide();
+	}
+}); 
+});
